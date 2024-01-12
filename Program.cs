d6c0d1432db0f972bd064e6d484e6375c7426eae
Somie1 <somelezesipayile@gmail.com>
2024-01-12 14:51:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Someleze");
+
+            //Freeze console (this is a comment, its a note to self in case i forget)
+            Console.ReadLine();
+        }
+    }
+}
